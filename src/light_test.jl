5705b7c03ad715e9ea5b8215d10f387dc5f09e50
ccoubard <chrislain.coubard@centralesupelec.fr>
2024-06-24 17:38:49 +0200

    Minor changes

--- src/light_test.jl
+++ src/light_test.jl
@@ -1,6 +1,7 @@
 using PlotlyJS
 include("parameters_light.jl")
 using Statistics
+using DelimitedFiles
 
 function computelight!(arrLight, I0, ke, dx, nx,pop)
     arrLight[1] = I0
@@ -13,10 +14,6 @@ end
 
 function computerespiration(RD, RL, I, n, Ik)
     return RD + (RL - RD) * (I^n/(I^n + Ik^n))
-end
-
-function computegrowthrate!(µ,i, I, R, k, sigma, tau, kd, kr)
-    µ[i] = (k*sigma*I) / (1 + tau*sigma*I + (kd/kr)*tau*(sigma*I)^2) - R 
 end
 
 function updatemu!(µ, RD, RL, I, n, Ik, k, sigma, tau, kd, kr, nx, pop)
@@ -34,7 +31,7 @@ function updateheight!(pop,µ,dt)
     end
 end
 
-function smootharray!(pop, nx)
+function smootharray!(pop, nx, dx)
     for i in 1:nx-1
         if pop[i] > dx
             pop[i+1] = pop[i+1] + pop[i] -dx
@@ -43,42 +40,48 @@ function smootharray!(pop, nx)
     end
 end
 
-# sp = SpaceParams()
+
+
 tp = TimeParams()
 mp = ModelParams()
 
-x = 1.5e-4 
-nx = 100
+x = 1e-4 
+nx = 1000
 dx = x/nx
 xplt = 0:dx:x
 
-
-space = zeros(nx)
-space[1:10] .= mp.ke
 DLI = zeros(nx)
 light_save = zeros(tp.n_save, nx)
 µ = zeros(nx)
+
 height = zeros(nx)
-height[1:10] .= dx
-mumean_save = zeros(n_save)
+height[1:30] .= dx
+mumean_save = zeros(tp.n_save)
 µ_save = zeros(tp.n_save, nx)
 time_save = zeros(tp.n_save)
+pop_save = zeros(tp.n_save, nx)
 height_save = zeros(tp.n_save)
+height_mean = zeros(tp.n_save)
+height_mean[1] = 3e-6
 #Random testing
-print(tp.n_inner)
 
 for time_step in 1:tp.n_save
     for i_inner in 1:tp.n_inner
         computelight!(DLI, mp.I0, mp.ke, dx, nx, height)
         updatemu!(µ, mp.RD, mp.RL, DLI, mp.n, mp.Ik,mp.k, mp.sigma, mp.tau, mp.kd, mp.kr, nx, height)
         updateheight!(height, µ, tp.dt)
-        smootharray!(height, nx)
+        smootharray!(height, nx, dx)
 
+    end
+    if time_step != 1
+        mumean = mean(µ)
+        height_mean[time_step] = mumean * height_mean[time_step-1] * tp.n_inner + height_mean[time_step-1]
     end
     time_save[time_step] = tp.dt*time_step*tp.n_inner/3600
     height_save[time_step] = sum(height)
     µ_save[time_step,:] = µ
     light_save[time_step,:] = DLI
+    pop_save[time_step,:] = height
 end
 
 
@@ -87,13 +90,13 @@ end
 
 layout1 = Layout(
     title = "Light attenuation",
-    xaxis_title = "Height (µm)",
+    xaxis_title = "depth (m)",
     yaxis_title = "light intensity (µmol*m-2*s-1)"
 )
 
 layout2 = Layout(
     title = "growth rate variation over height of biofilm", 
-    xaxis_title = "height (mm)", 
+    xaxis_title = "depth (m)", 
     yaxis_title = "growth rate (d-1)"
 )
 
@@ -122,23 +125,22 @@ end
 
 plt = plot(
     scatter(y = DLI, x = xplt*1e6, mode = "line", line = attr(color = "blue")), layout1)
-# display(plt)
+display(plt)
 
-plt2 = plot(scatter(y = µ*86400, x = xplt*1e4, mode = "markers"), layout2)
-# display(plt2)
+plt2 = plot(scatter(y = µ, x = xplt, mode = "markers"), layout2)
+display(plt2)
 
 plt3 = plot(scatter(x = time_save, y = height_save, mode = "markers"), layout3)
-# display(plt3)
-# ppp = plot(scatter(y=height2, x = 0:tp.n_save, mode = "line"), layout3)
-# display(ppp)
-
-plt5 = plot(bar(x = time_save, y = height_save))
-# display(plt5)
+display(plt3)
 
 plt_light = plot(traces)
-# display(plt_light)
+display(plt_light)
 
 plt_mu = plot(traces_µ)
 display(plt_mu)
-
-print(height[1:30])
+# print(height[1:30])
+muuu = filter(x -> x!=0, µ)
+println(mean(muuu)*86400)
+# writedlm("mu_save.csv", µ_save, ",")
+# writedlm("height_save.csv", height_save, ",")
+# writedlm("pop_save.csv",pop_save,",")
