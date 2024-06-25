2af436482f33c75c29a31a96a078dda96bb19d46
ccoubard <chrislain.coubard@centralesupelec.fr>
2024-06-25 16:49:29 +0200

    minor changes

--- src/light_test.jl
+++ src/light_test.jl
@@ -45,42 +45,35 @@ end
 tp = TimeParams()
 mp = ModelParams()
 
+##Space discretization
 x = 1e-4 
 nx = 1000
 dx = x/nx
 xplt = 0:dx:x
 
-DLI = zeros(nx)
-light_save = zeros(tp.n_save, nx)
+LI = zeros(nx)
 µ = zeros(nx)
 
 height = zeros(nx)
-height[1:30] .= dx
+height[1:100] .= dx
 mumean_save = zeros(tp.n_save)
 µ_save = zeros(tp.n_save, nx)
 time_save = zeros(tp.n_save)
 pop_save = zeros(tp.n_save, nx)
 height_save = zeros(tp.n_save)
-height_mean = zeros(tp.n_save)
-height_mean[1] = 3e-6
-#Random testing
+
 
 for time_step in 1:tp.n_save
     for i_inner in 1:tp.n_inner
-        computelight!(DLI, mp.I0, mp.ke, dx, nx, height)
-        updatemu!(µ, mp.RD, mp.RL, DLI, mp.n, mp.Ik,mp.k, mp.sigma, mp.tau, mp.kd, mp.kr, nx, height)
+        computelight!(LI, mp.I0, mp.ke, dx, nx, height)
+        updatemu!(µ, mp.RD, mp.RL, LI, mp.n, mp.Ik,mp.k, mp.sigma, mp.tau, mp.kd, mp.kr, nx, height)
         updateheight!(height, µ, tp.dt)
         smootharray!(height, nx, dx)
 
     end
-    if time_step != 1
-        mumean = mean(µ)
-        height_mean[time_step] = mumean * height_mean[time_step-1] * tp.n_inner + height_mean[time_step-1]
-    end
     time_save[time_step] = tp.dt*time_step*tp.n_inner/3600
     height_save[time_step] = sum(height)
     µ_save[time_step,:] = µ
-    light_save[time_step,:] = DLI
     pop_save[time_step,:] = height
 end
 
@@ -106,25 +99,29 @@ layout3 = Layout(
     yaxis_title = "height (m)"
 )
 
-
-traces = Vector{GenericTrace}(undef, tp.n_save)
-for i in 1:tp.n_save
-    t = i*tp.dt*tp.n_inner/3600
-    traces[i] = scatter(x = xplt, y = light_save[i,:], 
-        mode = "line", name = "$t h",
-        line = attr(color = "red", width = 1))
-end
-
 traces_µ = Vector{GenericTrace}(undef, tp.n_save)
 for i in 1:tp.n_save
-    t = i*tp.dt*tp.n_inner/3600
+    t = time_save[i]
     traces_µ[i] = scatter(x = xplt, y = µ_save[i,:], 
         mode = "line", name = "$t h",
         line = attr(color = "red", width = 1))
 end
 
+traces_test = Vector{GenericTrace}(undef, tp.n_save)
+for i in 1:tp.n_save
+    t = time_save[i]
+    mu_clean = filter!(x->x!=0,µ_save[i,:]) 
+    traces_test[i] = scatter(x = eachindex(mu_clean), y = mu_clean,
+        mode = "line", name = "$t h", 
+        line = attr(color = "red", width = 1))
+end
+
+
+
+
+
 plt = plot(
-    scatter(y = DLI, x = xplt*1e6, mode = "line", line = attr(color = "blue")), layout1)
+    scatter(y = LI, x = xplt*1e6, mode = "line", line = attr(color = "blue")), layout1)
 display(plt)
 
 plt2 = plot(scatter(y = µ, x = xplt, mode = "markers"), layout2)
@@ -132,9 +129,6 @@ display(plt2)
 
 plt3 = plot(scatter(x = time_save, y = height_save, mode = "markers"), layout3)
 display(plt3)
-
-plt_light = plot(traces)
-display(plt_light)
 
 plt_mu = plot(traces_µ)
 display(plt_mu)
@@ -144,3 +138,8 @@ println(mean(muuu)*86400)
 # writedlm("mu_save.csv", µ_save, ",")
 # writedlm("height_save.csv", height_save, ",")
 # writedlm("pop_save.csv",pop_save,",")
+
+layout_test = Layout(xaxis_range = 1e-4)
+
+plt_test = plot(traces_test,layout_test)
+display(plt_test)
