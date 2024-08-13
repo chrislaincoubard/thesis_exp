f615752675ed7f66c2d5eec88d79945424b8296d
ccoubard <chrislain.coubard@centralesupelec.fr>
2024-08-13 10:02:27 +0200

    WIP

--- src/model_parameters.jl
+++ src/model_parameters.jl
@@ -2,9 +2,9 @@ using Parameters
 
 @with_kw struct TimeParams @deftype Number
     t_tot = 7*86400 #(s)
-    dt = 150
+    dt = 1
     n_iter::Int = round(Int, t_tot/dt)
-    n_save = 32
+    n_save = 64
     n_inner::Int = n_iter/n_save
 end
 
@@ -24,14 +24,22 @@ end
     sigma = 1.59e-2 #cross section of PSII (m2*µmol-1)
     n = 3 #switching rate between RL and RD (dimentionless)
     Ik = 50 #half saturating light for light respiration (µmol*m-2*s-1)
-    RD = 9.15e-8 #respiration in dark phase (s-1)
+    RD = 1.73e-8 #respiration in dark phase (s-1)
     RL = 1.78e-6 #respiration in light phase (s-1)
     rho = 1.4e2  #biomass volumic mass (kg*m-3)
 end
 
 @with_kw struct GasesParams @deftype Number
     D_oxygen = 0.68*1.99e-9 #coef de diffusion de l'O2 dans le biofilm = 68% du coef de diffusion de l'O2 dans l'eau (m/s)
-    O2atm = 0.274 #Valeur d'un milieu solide saturé en 02, assimilé au biofilm (mol/m3)
+    D_CO2 = 1.18e-9 #coefficient de diffusion du CO2 assimilé aà celui des ions bicarbonate
+    O2surf = 0.274 #Valeur d'un milieu solide saturé en 02, assimilé au biofilm (mol/m3)
     VO2_x = 1.125 #Coefficient stoechiométrique de production d'O2 par rapport à la biomasse
     Mx = 0.024 # C-masse molaire de la biomasse (kgx/C-mol)
+    u = 5.4e-6 # Velocity of O2 (m/s)
+    PCo2 = 1.013 #partial pressure of Co2 in air (KPa)
+    HCo2 = 0.042 #Henry's constant for CO2 (mol/m3/KPa)
+    ka1 = 10^(-6.3) #dissociation coefficient of carbonic acid
+    ka2 = 10^(-10.3) #dissociation coefficient of bicabronate ion
+    phs = 7 #pH de surface du Biofilm
+    
 end
